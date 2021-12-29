940adb20ae3abe1d19944d7c50a92eb4c42a57d9
uq-hong <80738142+uq-hong@users.noreply.github.com>
2021-12-29 15:20:34 +0900

    feat: キークリックのイベントの追加とコンポーネントの追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,33 @@
+import { useState, useEffect, useCallback } from "react"
 import type { NextPage } from "next"
-import { Keyboard } from "components/index"
+import { Keyboard, WordContent } from "components/index"
+import style from "styles/Home.module.scss"
 
 const Home: NextPage = () => {
+  const [inputKey, setInputKey] = useState("")
+  const [inputValue, setInputValue] = useState("")
+
+  const handleKeydown = useCallback((e: KeyboardEvent) => {
+    setInputValue((inputValue) => inputValue + e.key)
+    setInputKey(e.key)
+    setTimeout(() => {
+      setInputKey("")
+    }, 100)
+    return
+  }, [])
+
+  useEffect(() => {
+    document.addEventListener("keydown", (e) => handleKeydown(e), false)
+
+    return () => {
+      document.removeEventListener("keydown", (e) => handleKeydown(e), false)
+    }
+  }, [])
   return (
-    <>
-      <Keyboard />
-    </>
+    <main className={style.main}>
+      <WordContent inputKey={inputKey} />
+      <Keyboard inputKey={inputKey} />
+    </main>
   )
 }
 
