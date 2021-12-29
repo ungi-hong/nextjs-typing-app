e6efb3277b10a167cc1a83a74143aed0cf79807e
uq-hong <80738142+uq-hong@users.noreply.github.com>
2021-12-30 08:41:51 +0900

    fix: キーレイウトの修正

--- utils/keysLayout.ts
+++ utils/keysLayout.ts
@@ -1,76 +1,75 @@
 interface Key {
   id: string
   value: string
-  keyName: string
 }
 
-interface keyRow {
-  keysRowId: string
-  keysRowItems: Key[]
+interface keyLine {
+  keyLineId: string
+  keyItems: Key[]
 }
 
 export default class KeysLayout {
-  static readonly keysList: Array<keyRow> = [
+  static readonly keys: Array<keyLine> = [
     {
-      keysRowId: "firstRow",
-      keysRowItems: [
-        { id: "Digit1", value: "1", keyName: "1" },
-        { id: "Digit2", value: "2", keyName: "2" },
-        { id: "Digit3", value: "3", keyName: "3" },
-        { id: "Digit4", value: "4", keyName: "4" },
-        { id: "Digit5", value: "5", keyName: "5" },
-        { id: "Digit6", value: "6", keyName: "6" },
-        { id: "Digit7", value: "7", keyName: "7" },
-        { id: "Digit8", value: "8", keyName: "8" },
-        { id: "Digit9", value: "9", keyName: "9" },
-        { id: "Digit0", value: "0", keyName: "0" },
+      keyLineId: "firstLine",
+      keyItems: [
+        { id: "Digit1", value: "1" },
+        { id: "Digit2", value: "2" },
+        { id: "Digit3", value: "3" },
+        { id: "Digit4", value: "4" },
+        { id: "Digit5", value: "5" },
+        { id: "Digit6", value: "6" },
+        { id: "Digit7", value: "7" },
+        { id: "Digit8", value: "8" },
+        { id: "Digit9", value: "9" },
+        { id: "Digit0", value: "0" },
       ],
     },
     {
-      keysRowId: "secondRow",
-      keysRowItems: [
-        { id: "keyQ", value: "q", keyName: "Q" },
-        { id: "keyW", value: "w", keyName: "W" },
-        { id: "keyE", value: "e", keyName: "E" },
-        { id: "keyR", value: "r", keyName: "R" },
-        { id: "keyT", value: "t", keyName: "T" },
-        { id: "keyY", value: "y", keyName: "Y" },
-        { id: "keyU", value: "u", keyName: "U" },
-        { id: "keyI", value: "i", keyName: "I" },
-        { id: "keyO", value: "o", keyName: "O" },
-        { id: "keyP", value: "p", keyName: "P" },
+      keyLineId: "secondLine",
+      keyItems: [
+        { id: "keyQ", value: "q" },
+        { id: "keyW", value: "w" },
+        { id: "keyE", value: "e" },
+        { id: "keyR", value: "r" },
+        { id: "keyT", value: "t" },
+        { id: "keyY", value: "y" },
+        { id: "keyU", value: "u" },
+        { id: "keyI", value: "i" },
+        { id: "keyO", value: "o" },
+        { id: "keyP", value: "p" },
       ],
     },
     {
-      keysRowId: "thirdRow",
-      keysRowItems: [
-        { id: "keyA", value: "a", keyName: "A" },
-        { id: "keyS", value: "s", keyName: "S" },
-        { id: "keyD", value: "d", keyName: "D" },
-        { id: "keyF", value: "f", keyName: "F" },
-        { id: "keyG", value: "g", keyName: "G" },
-        { id: "keyH", value: "h", keyName: "H" },
-        { id: "keyJ", value: "j", keyName: "J" },
-        { id: "keyK", value: "k", keyName: "K" },
-        { id: "keyL", value: "l", keyName: "L" },
-        { id: "keyENTER", value: "Enter", keyName: "ENTER" },
+      keyLineId: "thirdLine",
+      keyItems: [
+        { id: "keyA", value: "a" },
+        { id: "keyS", value: "s" },
+        { id: "keyD", value: "d" },
+        { id: "keyF", value: "f" },
+        { id: "keyG", value: "g" },
+        { id: "keyH", value: "h" },
+        { id: "keyJ", value: "j" },
+        { id: "keyK", value: "k" },
+        { id: "keyL", value: "l" },
+        { id: "keyENTER", value: "Enter" },
       ],
     },
     {
-      keysRowId: "fourthRow",
-      keysRowItems: [
-        { id: "keyZ", value: "z", keyName: "Z" },
-        { id: "keyX", value: "x", keyName: "X" },
-        { id: "keyC", value: "c", keyName: "C" },
-        { id: "keyV", value: "v", keyName: "V" },
-        { id: "keyB", value: "b", keyName: "B" },
-        { id: "keyN", value: "n", keyName: "N" },
-        { id: "keyM", value: "m", keyName: "M" },
+      keyLineId: "fourthLine",
+      keyItems: [
+        { id: "keyZ", value: "z" },
+        { id: "keyX", value: "x" },
+        { id: "keyC", value: "c" },
+        { id: "keyV", value: "v" },
+        { id: "keyB", value: "b" },
+        { id: "keyN", value: "n" },
+        { id: "keyM", value: "m" },
       ],
     },
     {
-      keysRowId: "fifthRow",
-      keysRowItems: [{ id: "keySpaceBar", value: " ", keyName: "" }],
+      keyLineId: "fifthLine",
+      keyItems: [{ id: "keySpaceBar", value: " " }],
     },
   ]
 }
